3d9e0b42a64b8dd1c7e7495fb1a16ecf971f34ea
scopophobic <adithyanmadhu1234@gmail.com>
2026-06-02 06:56:25 +0530

    update the sha256

--- Formula/zerotest.rb
+++ Formula/zerotest.rb
@@ -6,7 +6,7 @@ class Zerotest < Formula
   on_macos do
     on_arm do
       url "https://github.com/scopophobic/ZeroTest/releases/download/v0.1.2/zerotest-darwin-arm64.tar.gz"
-      sha256 "293981dd8ed9c993382f8ae2a678e93be0e0c5c169419d371fd2a2506b79fc06"
+      sha256 "293981dd8ed9c993382f8ae2a678e93be0e0c5c169419d371fd2a2506b79fc06" 
       def install
         bin.install "zerotest-darwin-arm64" => "zerotest"
       end
@@ -14,7 +14,7 @@ class Zerotest < Formula
 
     on_intel do
       url "https://github.com/scopophobic/ZeroTest/releases/download/v0.1.2/zerotest-darwin-amd64.tar.gz"
-      sha256 "ae54d9206ce042385a0725f9fc84f10a4a6d7d5995a13cc8fd28294c52d9ef5c"
+      sha256 "5a30c240791db532325dc6975db0177df880e216a56388fa0c922c685217fed2"
 
       def install
         bin.install "zerotest-darwin-amd64" => "zerotest"
@@ -24,7 +24,7 @@ class Zerotest < Formula
 
   on_linux do
     url "https://github.com/scopophobic/ZeroTest/releases/download/v0.1.2/zerotest-linux-amd64.tar.gz"
-    sha256 "6091984c2f94310c383b77c30a9ffc644cb1f646e22989a4e149609a64ecb963"
+    sha256 "ae1b4320948fecc0d5926ef1fb115cb660a13e640e72f5699071c3ab1b529300"
     def install
       bin.install "zerotest-linux-amd64" => "zerotest"
     end
